57e5fff1dc5d8c7c02474d900ba20a5c6e0af17e
ycj <zerogod708705@hotmail.com>
2020-04-01 16:32:23 +0800

    finish

--- TwoSum/hpp/two_sum.hpp
+++ TwoSum/hpp/two_sum.hpp
@@ -3,3 +3,26 @@
 #include <cstring>
 #include <iostream>
 #include <vector>
+using namespace std;
+
+class TwoSum {
+public:
+    TwoSum(){};
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        int a=0;
+        int b=0;
+        for(int i =0;i<nums.size();i++){
+            a = nums[i];
+            for(int j=i+1;j<nums.size();j++){
+                b = nums[j];
+                if(target==(a+b)){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
--- TwoSum/main.cpp
+++ TwoSum/main.cpp
@@ -1,6 +1,30 @@
 #include "hpp/two_sum.hpp"
 
+class TestCenter{
+  public:
+  int isSame(int expect, int input){
+    if(expect==input){
+      return 1;
+    }
+    else{
+      return 0;
+    }
+  }
+};
 
 int main(){
+  TestCenter TC;
 std::cout<<" Two sum :\n";
+vector<int> input{2, 7, 11, 15};
+int target = 9;
+vector<int> ans{0,1};
+TwoSum TS;
+vector<int>result = TS.twoSum(input,target);
+if(TC.isSame(ans[0],result[0])){
+  cout<<"PASS!\n";
+}
+else{
+  cout<<"FAIL!\n";
+}
+return 0;
 }
